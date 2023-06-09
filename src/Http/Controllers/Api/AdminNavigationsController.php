940176f60cd0a631a851bff899f85e7d355def84
Lukas Roth <mail@lukasroth.net>
2023-06-09 11:30:20 +0200

    added custom content types to navigation

--- src/Http/Controllers/Api/AdminNavigationsController.php
+++ src/Http/Controllers/Api/AdminNavigationsController.php
@@ -4,6 +4,7 @@ namespace Motor\Admin\Http\Controllers\Api;
 
 use Illuminate\Http\JsonResponse;
 use Motor\Admin\Http\Controllers\ApiController;
+use Motor\ContentType\Models\CustomContentType;
 
 /**
  * Class AdminNavigationsController
@@ -17,7 +18,30 @@ class AdminNavigationsController extends ApiController
     {
         $items = config('motor-admin-navigation.items');
         ksort($items);
-
+        if (class_exists(CustomContentType::class)) {
+            $navigation_position = 200;
+            $items[$navigation_position] = [
+                'slug' => 'custom-content-type',
+                'icon' => 'file',
+                'route' => null,
+                'roles' => ['SuperAdmin'],
+                'permissions' => [],
+                'name' => 'motor-content-type.content-types.content_types',
+                'items' => []
+            ];
+            CustomContentType::all()->each(function (CustomContentType $content_type) use (&$navigation_position, &$items) {
+                $items[200]['items'][$navigation_position] = [
+                    'slug' => $content_type->name,
+                    'icon' => 'fa fa-plus',
+                    'route' => 'admin.motor-content-type.show.' . $content_type->id,
+                    'roles' => ['SuperAdmin'],
+                    'permissions' => [],
+                    'aliases' => [],
+                    'name' => $content_type->name,
+                ];
+                $navigation_position++;
+            });
+        }
         return response()->json(['data' => $items]);
     }
 }
