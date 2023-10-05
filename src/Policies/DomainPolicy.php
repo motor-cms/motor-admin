1093527020f0bfce30b217ee82034b6a29dd1c97
Kuntz Loïc <54593969+kuntzloic@users.noreply.github.com>
2023-10-05 15:15:01 +0200

    Added DomainPolicy

---
+++ src/Policies/DomainPolicy.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Motor\Admin\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+use Motor\Admin\Models\Domain;
+use Motor\Admin\Models\User;
+
+class DomainPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Perform pre-authorization checks.
+     *
+     * @param  string  $ability
+     * @return void|bool
+     */
+    public function before(User $user, $ability)
+    {
+        if ($user->hasRole(['SuperAdmin', 'Editor', 'Writer'])) {
+            return true;
+        }
+    }
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return $user->hasPermissionTo('domain.read');
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @return mixed
+     */
+    public function view(User $user, Domain $domain)
+    {
+        return $user->hasPermissionTo('domain.read');
+    }
+
+    /**
+     * Determine whether the user can create models.
+     *
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return $user->hasPermissionTo('domain.write');
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @return mixed
+     */
+    public function update(User $user, Domain $domain)
+    {
+        return $user->hasPermissionTo('domain.write');
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @return mixed
+     */
+    public function delete(User $user, Domain $domain)
+    {
+        return $user->hasPermissionTo('domain.delete');
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @return mixed
+     */
+    public function restore(User $user, Domain $domain)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @return mixed
+     */
+    public function forceDelete(User $user, Domain $domain)
+    {
+        //
+    }
+}
