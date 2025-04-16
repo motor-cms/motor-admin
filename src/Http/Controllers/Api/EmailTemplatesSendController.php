851b676f1025e381d6049360d87589d05de7bfa3
Lukas Roth <mail@lukasroth.net>
2025-04-16 14:45:59 +0200

    EN-1432 removed client filter from sending mails cause slugs are unique and couldnt be found for other client, see TODO

--- src/Http/Controllers/Api/EmailTemplatesSendController.php
+++ src/Http/Controllers/Api/EmailTemplatesSendController.php
@@ -23,10 +23,13 @@ class EmailTemplatesSendController extends Controller
     public function send(EmailTemplateSendPostRequest $request, EmailTemplateService $emailTemplateService): JsonResponse
     {
         $emailTemplate = EmailTemplate::where('slug', $request->get('slug'))
-            ->where('client_id', $request->get('client_id'))
             ->where('language_id', $request->get('language_id'))
             ->whereNull('deleted_by')
             ->first();
+            // TODO: removed client filter cause template could not be found cause it was not found for client, cause the slugs must be unique in mysql
+            // we need to migrate the DB to non unique slugs and handle the client_id slug uniqueness over the request
+            // client->where('client_id', $request->get('client_id'))
+
 
         if (! is_null($emailTemplate)) {
             $data = Mail::send(new Email($emailTemplate, $request->all()));
