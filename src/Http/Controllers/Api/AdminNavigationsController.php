8ef0dd0154ee7e5fbcf1d4a5a239aa9dc0e3b70f
Reza Esmaili <github@dfox.info>
2026-03-02 15:52:31 +0100

    refactor: disable custom content type navigation block
    Commented out the dynamic CustomContentType navigation injection
    in AdminNavigationsController as it is no longer needed.
    
    Part of EN-1812: API v2 Refactoring

--- src/Http/Controllers/Api/AdminNavigationsController.php
+++ src/Http/Controllers/Api/AdminNavigationsController.php
@@ -22,31 +22,31 @@ class AdminNavigationsController extends ApiController
     {
         $items = config('motor-admin-navigation.items');
         ksort($items);
-        $customContentQuery = CustomContentType::where('type', 'page')->get();
-        if (class_exists(CustomContentType::class) && ! $customContentQuery->isEmpty()) {
-            $navigation_position = 200;
-            $items[$navigation_position] = [
-                'slug'        => 'custom-content-type',
-                'icon'        => 'file',
-                'route'       => null,
-                'roles'       => ['SuperAdmin'],
-                'permissions' => [],
-                'name'        => 'motor-content-type.content-types.content_types',
-                'items'       => [],
-            ];
-            $customContentQuery->each(function (CustomContentType $content_type) use (&$navigation_position, &$items) {
-                $items[200]['items'][$navigation_position] = [
-                    'slug'        => $content_type->name,
-                    'icon'        => 'fa fa-plus',
-                    'route'       => 'admin.motor-content-type.'.$content_type->id,
-                    'roles'       => ['SuperAdmin'],
-                    'permissions' => [],
-                    'aliases'     => [],
-                    'name'        => $content_type->name,
-                ];
-                $navigation_position++;
-            });
-        }
+        //$customContentQuery = CustomContentType::where('type', 'page')->get();
+        //if (class_exists(CustomContentType::class) && ! $customContentQuery->isEmpty()) {
+        //    $navigation_position = 200;
+        //    $items[$navigation_position] = [
+        //        'slug'        => 'custom-content-type',
+        //        'icon'        => 'file',
+        //        'route'       => null,
+        //        'roles'       => ['SuperAdmin'],
+        //        'permissions' => [],
+        //        'name'        => 'motor-content-type.content-types.content_types',
+        //        'items'       => [],
+        //    ];
+        //    $customContentQuery->each(function (CustomContentType $content_type) use (&$navigation_position, &$items) {
+        //        $items[200]['items'][$navigation_position] = [
+        //            'slug'        => $content_type->name,
+        //            'icon'        => 'fa fa-plus',
+        //            'route'       => 'admin.motor-content-type.'.$content_type->id,
+        //            'roles'       => ['SuperAdmin'],
+        //            'permissions' => [],
+        //            'aliases'     => [],
+        //            'name'        => $content_type->name,
+        //        ];
+        //        $navigation_position++;
+        //    });
+        //}
 
         return response()->json(['data' => $items]);
     }
