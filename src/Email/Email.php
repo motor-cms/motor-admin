bc7c8572cd04aba670cccba7f715fcebbe37b2ba
Reza Esmaili <github@dfox.info>
2026-03-22 00:29:09 +0100

    chore: remove PII-leaking debug logs from Email class
    Remove Log::info calls that logged full request data and email
    content, which could contain personally identifiable information.

--- src/Email/Email.php
+++ src/Email/Email.php
@@ -8,7 +8,6 @@ use Illuminate\Mail\Mailables\Attachment;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Motor\Admin\Models\EmailTemplate;
 
@@ -55,17 +54,14 @@ class Email extends Mailable
      */
     protected function content(): Content
     {
-        Log::info('EmailTemplateSendPostRequest: '.json_encode($this->requestData));
         $this->contentHtml = $this->requestData['body_html'] ?? '';
 
         if ($this->emailTemplate['has_body_html'] && ! empty($this->emailTemplate['body_html'])) {
             // Set text from email template
             $this->contentHtml = $this->emailTemplate['body_html'];
-            Log::info('EmailTemplateSendPostRequest: contentHTML = '.$this->contentHtml);
         }
 
         $this->contentText = $this->requestData['body_text'] ?? $this->emailTemplate['body_text'] ?? '';
-        Log::info('EmailTemplateSendPostRequest: contentText = '.$this->contentText);
 
         // Replace placeholders like {FOO}
         if (isset($this->requestData['text_replace_data']) && $this->requestData['text_replace_data']) {
